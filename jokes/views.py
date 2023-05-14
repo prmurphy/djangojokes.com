6591059b994d933471b78dc94d9dfc7ec25d9ed9
prmurphy <prmurphy27@gmail.com>
2023-05-14 15:21:37 -0500

    adding create/update options for jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,10 +1,18 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 
 from .models import Joke
 
 class JokeDetailView(DetailView):
     model = Joke
 
-    
+
 class JokeListView(ListView):
     model = Joke
+
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']

92b65aa697d4877d08b85491e9fac2971bbb2c56
prmurphy <prmurphy27@gmail.com>
2023-05-20 15:30:06 -0500

    adding slug generating function

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
     # Local apps
     'pages.apps.PagesConfig',
     'jokes.apps.JokesConfig',
--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
+
+    path('admin/', admin.site.urls),   
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ]

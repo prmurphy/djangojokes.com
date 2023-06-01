fa4d46d5b360622f770556e2b2cf18b8e3647514
prmurphy <prmurphy27@gmail.com>
2023-05-31 21:49:49 -0500

    added application page

--- djangojokes/urls.py
+++ djangojokes/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
 
-    path('admin/', admin.site.urls),   
+    path('admin/', admin.site.urls),  
+    path('jobs/', include('jobs.urls')), 
     path('jokes/', include('jokes.urls')),
     path('', include('pages.urls')),
 ]
